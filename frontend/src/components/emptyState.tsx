f9d933793f1bb2ffa8fd1b0797cd028e333c26e3
PradeepKshetri04 <pradeepkshetri459@gmail.com>
2026-09-16 17:18:17 +0545

    completed_Maybe

--- frontend/src/components/emptyState.tsx
+++ frontend/src/components/emptyState.tsx
@@ -5,9 +5,13 @@ export function EmptystateImage(){
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4 bg-white shadow-md">
                 <div className="border-2  m-4 rounded-lg border-slate-50 shadow-md ">
                     <div id="box1" className="grid-row p-4 bg-white text-black">
-                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>
-                        <div className="w-full h-15 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
-                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
+                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
+                        <div className="w-full h-5 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
+                    </div>
+                </div>
+                <div className="border-2  m-4 rounded-lg border-slate-50 shadow-md ">
+                    <div id="box1" className="grid-row p-4 bg-white text-black">
+                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
                         <div className="w-full h-5 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
                     </div>
                 </div>
@@ -21,81 +25,55 @@ export function EmptystateImage(){
                 </div>
                 <div className="border-2  m-4 rounded-lg border-slate-50 shadow-md ">
                     <div id="box1" className="grid-row p-4 bg-white text-black">
-                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>
-                        <div className="w-full h-15 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
-                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
+                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
                         <div className="w-full h-5 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
                     </div>
                 </div>
                 <div className="border-2  m-4 rounded-lg border-slate-50 shadow-md ">
                     <div id="box1" className="grid-row p-4 bg-white text-black">
-                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>
-                        <div className="w-full h-15 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
-                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
+                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
                         <div className="w-full h-5 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
                     </div>
                 </div>
                 <div className="border-2  m-4 rounded-lg border-slate-50 shadow-md ">
                     <div id="box1" className="grid-row p-4 bg-white text-black">
-                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>
-                        <div className="w-full h-15 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
-                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
+                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
                         <div className="w-full h-5 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
                     </div>
                 </div>
                 <div className="border-2  m-4 rounded-lg border-slate-50 shadow-md ">
                     <div id="box1" className="grid-row p-4 bg-white text-black">
-                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>
-                        <div className="w-full h-15 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
-                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
+                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
                         <div className="w-full h-5 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
                     </div>
                 </div>
                 <div className="border-2  m-4 rounded-lg border-slate-50 shadow-md ">
                     <div id="box1" className="grid-row p-4 bg-white text-black">
-                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>
-                        <div className="w-full h-15 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
-                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
+                        <div className="w-full h0 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
                         <div className="w-full h-5 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
                     </div>
                 </div>
                 <div className="border-2  m-4 rounded-lg border-slate-50 shadow-md ">
                     <div id="box1" className="grid-row p-4 bg-white text-black">
-                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>
-                        <div className="w-full h-15 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
-                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
+                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
                         <div className="w-full h-5 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
                     </div>
                 </div>
                 <div className="border-2  m-4 rounded-lg border-slate-50 shadow-md ">
                     <div id="box1" className="grid-row p-4 bg-white text-black">
-                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>
-                        <div className="w-full h-15 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
-                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
+                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
                         <div className="w-full h-5 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
                     </div>
                 </div>
                 <div className="border-2  m-4 rounded-lg border-slate-50 shadow-md ">
                     <div id="box1" className="grid-row p-4 bg-white text-black">
-                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>
-                        <div className="w-full h-15 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
-                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
+                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
                         <div className="w-full h-5 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
                     </div>
                 </div>
                 <div className="border-2  m-4 rounded-lg border-slate-50 shadow-md ">
                     <div id="box1" className="grid-row p-4 bg-white text-black">
-                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>
-                        <div className="w-full h-15 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
-                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
-                        <div className="w-full h-5 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
-                    </div>
-                </div>
-                <div className="border-2  m-4 rounded-lg border-slate-50 shadow-md ">
-                    <div id="box1" className="grid-row p-4 bg-white text-black">
-                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>
-                        <div className="w-full h-15 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
-                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
+                        <div className="w-full h-60 bg-slate-300 rounded-2xl transition-colors duration-500 ease-in-out"></div>                        <div className="w-full h-6 mt-3 bg-slate-300 h-4 rounded-2xl "></div>
                         <div className="w-full h-5 mt-3 bg-slate-300 h-4 rounded-2xl"></div>
                     </div>
                 </div>
--- frontend/src/hooks/productgrid.tsx
+++ frontend/src/hooks/productgrid.tsx
@@ -16,6 +16,7 @@ export  function ProductCardlist(){
 
    const [productsList, setProductList] = useState<productdetails[]>([]);
    const [isLoading, setisLoading] = useState(true);
+   const [singleproductsList, setSingleProductList] = useState<productdetails>();
 
     useEffect(() => {
         const fetchData = async() => {
@@ -35,18 +36,52 @@ export  function ProductCardlist(){
     if(isLoading){
         return <EmptystateImage />
     }
+
+
+    if(singleproductsList) {
+        return(
+        <div>
+            <div className='grid grid-cols-1 p-4' >
+                
+                <div className="m-auto">
+                    <button 
+                    type="button" 
+                    className="mb-4 rounded-xl bg-gray-500 text-white p-2"
+                    onClick={() => setSingleProductList(null)}
+                >close</button>
+                     <div key={singleproductsList.pid} className="overflow-hidden rounded-lg border border-slate-50 bg-white shadow-md transition-all duration-300 hover:scale-105">
+                            <img src={`http://localhost:4000/img/${singleproductsList.imgUrl}`} alt={singleproductsList.imgUrl} className='h-50 w-full p-4 object-contain' />
+                            <h3 className='mb-2  px-3 font-semibold text-gray-800'>{singleproductsList.Name}</h3>
+                             <div className='text-sm font-bold m-4'>
+                                <p> {singleproductsList.description}</p>
+                                <p className='text-green-500'>Price: {singleproductsList.price}</p>
+                                <p className='text-red-600'>Stock: {singleproductsList.stock}</p>
+                            </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        );
+    }
+
     return(
         <div>
-            <div className="container mx-auto p-6">
-                    <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            <div className="container mx-auto p-6 ">
+                    <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4 ">
                         {productsList.map((product) => (
-                            <div key={product.pid} className="overflow-hidden rounded-lg border border-slate-50 bg-white shadow-md">
+                            <div key={product.pid} className="overflow-hidden rounded-lg border border-slate-50 bg-white shadow-md transition-all duration-300 hover:scale-105">
                                 <img src={`http://localhost:4000/img/${product.imgUrl}`} alt={product.imgUrl} className='h-50 w-full p-4 object-contain' />
                                 <h3 className='mb-2  px-3 font-semibold text-gray-800'>{product.Name}</h3>
                                 <div className='text-sm font-bold m-4'>
-                                    <p> {product.description}</p>
-                                    <p className='text-green-500'>Price: {product.price}</p>
                                     <p className='text-red-600'>Stock: {product.stock}</p>
+                                    <button  type='button'
+                                    className='box-content border-1 rounded-xl border-slate-40 
+                                                text-white bg-black  mx-auto  p-1 w-full'
+                                                onClick= {() => setSingleProductList(product)}
+                                    >
+                                        view Details
+                                    </button>
                                 </div>
                             </div>
                         ))}
@@ -55,3 +90,6 @@ export  function ProductCardlist(){
         </div>
     );
 }
+
+//<p> {product.description}</p>
+//<p className='text-green-500'>Price: {product.price}</p>
